649419a1d7414e53d1ca9e988a479870e3b5d13c
Ajitesh Sakaray <sakarayajitesh@flashbox.in>
2018-10-16 17:43:22 +0530

    * add tips,ect

--- crud.py
+++ crud.py
@@ -11,7 +11,20 @@ db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
 
-class User(db.Model):
+class News(db.Model):
+    __tablename__ = 'news'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.TEXT,unique=True)
+    text = db.Column(db.TEXT)
+    image = db.Column(db.TEXT)
+
+    def __init__(self, title, text,image):
+        self.title = title
+        self.text = text
+        self.image = image
+
+class Tips(db.Model):
+    __tablename__ = 'tips'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.TEXT,unique=True)
     text = db.Column(db.TEXT)
@@ -23,35 +36,73 @@ class User(db.Model):
         self.image = image
 
 
+class Videos(db.Model):
+    __tablename__ = 'videos'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.TEXT,unique=True)
+    text = db.Column(db.TEXT)
+    videoId = db.Column(db.TEXT)
+
+    def __init__(self, title, text,videoid):
+        self.title = title
+        self.text = text
+        self.videoId = videoid
+
+
 class UserSchema(ma.Schema):
     class Meta:
         # Fields to expose
-        fields = ('title', 'text','image')
+        fields = ('id','title', 'text','image')
+
+
+class VideosSchema(ma.Schema):
+    class Meta:
+        # Fields to expose
+        fields = ('id','title', 'text','videoId')
 
 
 user_schema = UserSchema()
 users_schema = UserSchema(many=True)
 
 
+video_schema = VideosSchema()
+videos_schema = VideosSchema(many=True)
+
+
 # endpoint to create new user
-@app.route("/user", methods=["POST"])
-def add_user():
-    username = request.json['title']
-    email = request.json['text']
-
-    new_user = User(username, email)
-
-    db.session.add(new_user)
-    db.session.commit()
-
-    return user_schema.jsonify(new_user)
+# @app.route("/user", methods=["POST"])
+# def add_user():
+#     username = request.json['title']
+#     email = request.json['text']
+#     image = request.json['image']
+#
+#     new_user = User(username, email,image)
+#
+#     db.session.add(new_user)
+#     db.session.commit()
+#
+#     return user_schema.jsonify(new_user)
 
 
 # endpoint to show all users
 @app.route("/tips", methods=["GET"])
-def get_user():
-    all_users = User.query.all()
+def get_tips():
+    all_users = Tips.query.all()
     result = users_schema.dump(all_users)
+    return jsonify(result.data)
+
+
+@app.route("/news", methods=["GET"])
+def get_news():
+    all_users = News.query.all()
+    result = users_schema.dump(all_users)
+    return jsonify(result.data)
+
+
+@app.route("/videos", methods=["GET"])
+def get_videos():
+    all_users = Videos.query.all()
+    result = videos_schema.dump(all_users)
     return jsonify(result.data)
 
 
@@ -61,37 +112,39 @@ def get():
 
 
 # endpoint to get user detail by id
-@app.route("/user/<id>", methods=["GET"])
-def user_detail(id):
-    user = User.query.get(id)
-    return user_schema.jsonify(user)
+# @app.route("/user/<id>", methods=["GET"])
+# def user_detail(id):
+#     user = User.query.get(id)
+#     return user_schema.jsonify(user)
 
 
 # endpoint to update user
-@app.route("/user/<id>", methods=["PUT"])
-def user_update(id):
-    user = User.query.get(id)
-    username = request.json['username']
-    email = request.json['email']
-
-    user.email = email
-    user.username = username
-
-    db.session.commit()
-    return user_schema.jsonify(user)
+# @app.route("/user/<id>", methods=["PUT"])
+# def user_update(id):
+#     user = User.query.get(id)
+#     username = request.json['username']
+#     email = request.json['email']
+#
+#     user.email = email
+#     user.username = username
+#
+#     db.session.commit()
+#     return user_schema.jsonify(user)
 
 
 # endpoint to delete user
-@app.route("/user/<id>", methods=["DELETE"])
-def user_delete(id):
-    user = User.query.get(id)
-    db.session.delete(user)
-    db.session.commit()
-
-    return user_schema.jsonify(user)
+# @app.route("/user/<id>", methods=["DELETE"])
+# def user_delete(id):
+#     user = User.query.get(id)
+#     db.session.delete(user)
+#     db.session.commit()
+#
+#     return user_schema.jsonify(user)
 
 
 if __name__ == '__main__':
+    #app.run(debug=True)
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
+
 
