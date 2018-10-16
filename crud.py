d56892135023762332c5acd3f015647e1da93031
Ajitesh Sakaray <sakarayajitesh@flashbox.in>
2018-10-16 15:30:18 +0530

    make

--- crud.py
+++ crud.py
@@ -85,6 +85,6 @@ def user_delete(id):
 
 
 if __name__ == '__main__':
-    from os import environ
-    app.run(debug=False,host='0.0.0.0')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
 
