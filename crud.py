0f3563ae774dee9f060209fae83f32a11f2f6114
Ajitesh Sakaray <sakarayajitesh@flashbox.in>
2018-10-17 12:40:07 +0530

    * added 10 tips

--- crud.py
+++ crud.py
@@ -163,5 +163,5 @@ def tip_delete(id):
 
 if __name__ == '__main__':
     app.run(debug=True)
-    # port = int(os.environ.get("PORT", 5000))
-    # app.run(debug=True, host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
