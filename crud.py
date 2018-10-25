a1e40acc646c11ad0f91d9787ae292f43fe5770b
Ajitesh Sakaray <sakarayajitesh@flashbox.in>
2018-10-25 16:20:26 +0530

    * added checking for present records and adding them if not present

--- crud.py
+++ crud.py
@@ -183,7 +183,7 @@ def get_videos():
 def get():
     options = Options()
     options.headless = True
-    driver = webdriver.Chrome( executable_path=r'./chromedriver.exe',options=options)
+    driver = webdriver.Chrome( executable_path=r'./chromedriver',options=options)
     driver.get("https://www.msn.com/en-in/foodanddrink/foodnews")
     html = driver.page_source
     soup = BeautifulSoup(html, 'lxml')
